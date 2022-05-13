3a98f77c65e01d7d74edcaee5d242607970f2695
Maksym76 <kobzarenkomaksym@gmail.com>
2022-05-13 14:38:48 +0300

    task_4.1

--- kobzarenko_maksym_task_4_hw3.py
+++ kobzarenko_maksym_task_4_hw3.py
@@ -1,7 +1,7 @@
 # ask user count of earth day and hour (type number through ",")
-earth_day, earth_hour = map(int, input("write (earth_day, earth_hour): ").split(","))
+earth_day, earth_hour = input("write (earth_day, earth_hour): ").split(",")
 
 # translate earth day to solo
-solo = (earth_day + earth_hour / 24) * 1.02595675
+solo = (int(earth_day) + int(earth_hour) / 24) * 1.02595675
 
-print('count sol: ', round(sol, 2))
+print('count sol: ', round(solo, 2))
