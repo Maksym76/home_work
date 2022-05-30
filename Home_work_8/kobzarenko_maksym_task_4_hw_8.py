050c4de30b4964c14def586e68c8be8410d3abc1
Maksym76 <kobzarenkomaksym@gmail.com>
2022-05-30 15:59:31 +0300

    Home work № 8

--- Home_work_8/kobzarenko_maksym_task_4_hw_8.py
+++ Home_work_8/kobzarenko_maksym_task_4_hw_8.py
@@ -4,9 +4,9 @@ import datetime
 #  Создаем переменную в которой будет хранится текущая дата и время
 now = datetime.datetime.now()
 
-year = lambda x: x.year # С помощью "lamda" функции будем возвращать текущий год из экземпляра класса "datetime"
-month = lambda x: x.month #   С помощью "lamda" функции будем возвращать текущий месяц из экземпляра класса "datetime"
-day = lambda x: x.day #   С помощью "lamda" функции будем возвращать текущий день из экземпляра класса "datetime"
+year = lambda x: x.year  # С помощью "lamda" функции будем возвращать текущий год из экземпляра класса "datetime"
+month = lambda x: x.month  # С помощью "lamda" функции будем возвращать текущий месяц из экземпляра класса "datetime"
+day = lambda x: x.day  # С помощью "lamda" функции будем возвращать текущий день из экземпляра класса "datetime"
 
 print(year(now))
 
