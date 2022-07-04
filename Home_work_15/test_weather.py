8941f102ad78b4c2b4951555d07b5a0f105318be
Maksym76 <kobzarenkomaksym@gmail.com>
2022-07-04 21:15:14 +0300

    Home work № 15

--- Home_work_15/test_weather.py
+++ Home_work_15/test_weather.py
@@ -7,16 +7,16 @@ from get_weather import information_about_weather, pressure, temperature
 class TestWeather(TestCase):
 
     def test_lowe_pressure(self):
-        self.assertLess(pressure(), '1013.25')
+        self.assertLess(pressure(), 1013)
 
     def test_high_pressure(self):
-        self. assertGreater(pressure(), '1013.25')
+        self. assertGreater(pressure(), 1013)
 
     def test_negative_temperature(self):
-        self.assertLess(temperature(), '0')
+        self.assertLess(temperature(), 0)
 
     def test_positive_temperature(self):
-        self.assertLess(temperature(), '0')
+        self.assertGreater(temperature(), 0)
 
 if __name__ == '__mani__':
     main()
