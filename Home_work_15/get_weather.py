03a8e7f461eb2b025b2fa3a6aff2b29a36546d29
Maksym76 <kobzarenkomaksym@gmail.com>
2022-07-04 21:06:02 +0300

    Home work № 15

--- Home_work_15/get_weather.py
+++ Home_work_15/get_weather.py
@@ -4,28 +4,41 @@ base_url = 'http://api.openweathermap.org/data/2.5/weather?'
 
 api_key = 'd82759ebf4a4a5ed987117c4027b9dfa'  # if api_key not works, generate yours on website
 
+city_name = input('please fill up your city: ')
 
-def weather_in_city():
-    city_name = input('please fill up your city: ')
+complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+response = requests.get(complete_url)
+r_data = response.json()
 
-    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
-    response = requests.get(complete_url)
-    r_data = response.json()
+
+def information_about_weather():
     if r_data["cod"] != "404":
         weather_data = r_data['main']
         current_t = round(weather_data['temp'] - 273.15)
         current_p = weather_data["pressure"]
-        weather_discription = r_data["weather"]
-        weather_description = weather_discription[0]["description"]
-        return f'Weather data in {city_name.capitalize()}: current temperature {current_t} С, current pressure ' \
-               f'{current_p} hPa, {weather_description}'
+        weather_description = r_data["weather"]
+        weather_description = weather_description[0]["description"]
+        return f"Weather's data in {city_name.capitalize()}: current temperature {current_t} С, current pressure " \
+               f"{current_p} hPa, {weather_description}"
 
     else:
         return 'city not found'
 
 
+def pressure():
+    weather_pressure = r_data['main']
+    current_pressure = weather_pressure['pressure']
+    return current_pressure
+
+
+def temperature():
+    weather_temperature = r_data['main']
+    current_temperature = weather_temperature['temperature']
+    return current_temperature
+
+
 if __name__ == '__main__':
-    print(weather_in_city())
+    print(information_about_weather())
 
 """
 1. создать функцию(ии) для определения погоды по данным(Город).
--- Home_work_15/test_weather.py
+++ Home_work_15/test_weather.py
@@ -1,9 +1,22 @@
 from unittest import TestCase, main
 
-from get_weather import weather_in_city
+
+from get_weather import information_about_weather, pressure, temperature
 
 
 class TestWeather(TestCase):
 
-    def weather(self):
-        self.asser()
+    def test_lowe_pressure(self):
+        self.assertLess(pressure(), '1013.25')
+
+    def test_high_pressure(self):
+        self. assertGreater(pressure(), '1013.25')
+
+    def test_negative_temperature(self):
+        self.assertLess(temperature(), '0')
+
+    def test_positive_temperature(self):
+        self.assertLess(temperature(), '0')
+
+if __name__ == '__mani__':
+    main()
