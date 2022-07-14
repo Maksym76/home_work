e862db24019adf0722acd288df01368701c27389
Maksym76 <kobzarenkomaksym@gmail.com>
2022-07-14 10:17:49 +0300

    Home work № 16

--- Home_work_16/kobzarenko_maksym_task_3_hw_16.py
+++ Home_work_16/kobzarenko_maksym_task_3_hw_16.py
@@ -1,45 +1,41 @@
-""" National Bank of Ukraine
-Foreign exchange rate to UAH"""
-
 import requests
+import  datetime
+# currency = input('Write currency: ').upper()
+#
+# api_url = f'https://api.api-ninjas.com/v1/convertcurrency?want=UAH&have={currency}&amount=1'
+#
+# api_key = 'cEVBI1ovwpgTvN5gQyeWrw==XrhtDlSc2JvBAC43'
+#
+# response = requests.get(api_url, headers={'X-Api-Key': api_key}).json()
+#
+# if 'error' in response:
+#     print(f'Invalid currency name: {currency}')
+# else:
+#     print(f'''{currency}
+#
+# {response['new_amount']}''')
+#
+#
+# print(response)
+#
+# # {'new_amount': 29.82, 'new_currency': 'UAH', 'old_currency': 'EUR', 'old_amount': 1.0}
+#
+# # Write currency: rrrr
+# # {'error': 'Invalid currencies.'}
 
-import datetime
 
-try:
-    user_request: list = input(
-        "Write currency and date (yyyy.mm.dd., where: y-year, m-month, d-date. Default date = today's date)"
-        "\nExample:'USD 2020.04.22' or 'USD' \n").split(' ')
+currency = input('Write currency: ').upper()
+dat = datetime.date.today()
+data = datetime.date.strftime(dat, '%Y%m%d')
 
-    date_now = datetime.date.today()  # Create variable with today date
-    data: str = datetime.date.strftime(date_now, '%Y%m%d')  # Converting date to string
-    bank_currencies: list = ['AUD', 'CAD', 'CNY', 'HRK', 'CZK', 'DKK', 'HKD', 'HUF', 'INR', 'IDR', 'ILS', 'JPY', 'KZT',
-                             'KRW', 'MXN', 'MDL', 'NZD', 'NOK', 'RUB', 'SGD', 'ZAR', 'SEK', 'CHF', 'EGP', 'GBP', 'USD',
-                             'BYN', 'AZN', 'RON', 'TRY', 'XDR', 'BGN', 'EUR', 'PLN', 'DZD', 'BDT', 'AMD', 'DOP', 'IRR',
-                             'IQD', 'KGS', 'LBP', 'LYD', 'MYR', 'MAD', 'PKR', 'SAR', 'VND', 'THB', 'AED', 'TND', 'UZS',
-                             'TWD', 'TMT', 'RSD', 'TJS', 'GEL',
-                             'BRL']  # Create list with currencies which work the bank
-    currency: str = user_request[0].upper()  # Write currency which user chose
+api_url = f'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode={currency}&date={data}&json'
 
-    if len(user_request) == 2:
-        data: str = user_request[1].replace('.', '')
+response = requests.get(api_url).json()
 
-    api_url: str = f'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode={currency}&date={data}&json'
+if response == list():
+    print(f'Invalid currency name: {currency}')
+else:
+    print(f'''{currency}
 
-    response: list = requests.get(api_url).json()
-
-    if currency not in bank_currencies:
-        if currency == '':
-            print('Invalid currency name: None')
-        else:
-            print(f'Invalid currency name: {currency}')
-
-    elif response == list or 'message' in response[0]:
-        print(f'Invalid date {user_request[1]}')
-
-    else:
-        print(f'''{currency}
-    
 {response[0]['rate']}''')
-
-except BaseException:
-    print('System Error')
+    print(response)
