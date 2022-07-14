46531aad850e22820da35d94a76af694f1f3bcd7
Maksym76 <kobzarenkomaksym@gmail.com>
2022-07-14 19:40:09 +0300

    Home work № 16

--- Home_work_16/kobzarenko_maksym_task_3_hw_16.py
+++ Home_work_16/kobzarenko_maksym_task_3_hw_16.py
@@ -3,32 +3,30 @@ import requests
 import datetime
 
 try:
-    user_request = input(
+    user_request: list = input(
         "Write currency and date (yyyy.mm.dd., where: y-year, m-month, d-date. Default date = today's date)"
         "\nExample:'USD 2020.04.22' or 'USD' \n").split(' ')
 
     date_now = datetime.date.today()
-    data = datetime.date.strftime(date_now, '%Y%m%d')
-    bank_currencies = ['AUD', 'CAD', 'CNY', 'HRK', 'CZK', 'DKK', 'HKD', 'HUF', 'INR', 'IDR', 'ILS', 'JPY', 'KZT',
+    data: str = datetime.date.strftime(date_now, '%Y%m%d')
+    bank_currencies: list = ['AUD', 'CAD', 'CNY', 'HRK', 'CZK', 'DKK', 'HKD', 'HUF', 'INR', 'IDR', 'ILS', 'JPY', 'KZT',
                        'KRW', 'MXN', 'MDL', 'NZD', 'NOK', 'RUB', 'SGD', 'ZAR', 'SEK', 'CHF', 'EGP', 'GBP', 'USD', 'BYN',
                        'AZN', 'RON', 'TRY', 'XDR', 'BGN', 'EUR', 'PLN', 'DZD', 'BDT', 'AMD', 'DOP', 'IRR', 'IQD', 'KGS',
                        'LBP', 'LYD', 'MYR', 'MAD', 'PKR', 'SAR', 'VND', 'THB', 'AED', 'TND', 'UZS', 'TWD', 'TMT', 'RSD',
                        'TJS', 'GEL', 'BRL', 'XAU', 'XAG', 'XPT', 'XPD']
     currency = None
 
-    if user_request[0] == '':
-        currency = None
 
-    elif len(user_request) == 1:
-        currency = user_request[0].upper()
+    if len(user_request) == 1 and user_request[0] != '':
+        currency: str = user_request[0].upper()
 
     elif len(user_request) == 2:
-        currency = user_request[0].upper()
-        data = user_request[1].replace('.', '')
+        currency: str = user_request[0].upper()
+        data: str = user_request[1].replace('.', '')
 
-    api_url = f'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode={currency}&date={data}&json'
+    api_url: str = f'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode={currency}&date={data}&json'
 
-    response = requests.get(api_url).json()
+    response: list = requests.get(api_url).json()
 
     if currency not in bank_currencies:
         print(f'Invalid currency name: {currency}')
