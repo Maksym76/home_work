1b751ea82db734b38bfac8551bc10921a2859f6b
Maksym76 <kobzarenkomaksym@gmail.com>
2022-07-15 12:54:39 +0300

    Home work № 16

--- Home_work_16/kobzarenko_maksym_task_3_hw_16.py
+++ Home_work_16/kobzarenko_maksym_task_3_hw_16.py
@@ -1,3 +1,5 @@
+"""Foreign exchange rate to UAH"""
+
 import requests
 
 import datetime
@@ -7,21 +9,20 @@ try:
         "Write currency and date (yyyy.mm.dd., where: y-year, m-month, d-date. Default date = today's date)"
         "\nExample:'USD 2020.04.22' or 'USD' \n").split(' ')
 
-    date_now = datetime.date.today()
-    data: str = datetime.date.strftime(date_now, '%Y%m%d')
+    date_now = datetime.date.today()  # Create variable with today date
+    data: str = datetime.date.strftime(date_now, '%Y%m%d')  # Converting date to string
     bank_currencies: list = ['AUD', 'CAD', 'CNY', 'HRK', 'CZK', 'DKK', 'HKD', 'HUF', 'INR', 'IDR', 'ILS', 'JPY', 'KZT',
-                       'KRW', 'MXN', 'MDL', 'NZD', 'NOK', 'RUB', 'SGD', 'ZAR', 'SEK', 'CHF', 'EGP', 'GBP', 'USD', 'BYN',
-                       'AZN', 'RON', 'TRY', 'XDR', 'BGN', 'EUR', 'PLN', 'DZD', 'BDT', 'AMD', 'DOP', 'IRR', 'IQD', 'KGS',
-                       'LBP', 'LYD', 'MYR', 'MAD', 'PKR', 'SAR', 'VND', 'THB', 'AED', 'TND', 'UZS', 'TWD', 'TMT', 'RSD',
-                       'TJS', 'GEL', 'BRL', 'XAU', 'XAG', 'XPT', 'XPD']
-    currency = None
+                             'KRW', 'MXN', 'MDL', 'NZD', 'NOK', 'RUB', 'SGD', 'ZAR', 'SEK', 'CHF', 'EGP', 'GBP', 'USD',
+                             'BYN',
+                             'AZN', 'RON', 'TRY', 'XDR', 'BGN', 'EUR', 'PLN', 'DZD', 'BDT', 'AMD', 'DOP', 'IRR', 'IQD',
+                             'KGS',
+                             'LBP', 'LYD', 'MYR', 'MAD', 'PKR', 'SAR', 'VND', 'THB', 'AED', 'TND', 'UZS', 'TWD', 'TMT',
+                             'RSD',
+                             'TJS', 'GEL', 'BRL', 'XAU', 'XAG', 'XPT',
+                             'XPD']  # Create list with currencies which work the bank
+    currency: str = user_request[0].upper()  # Write currency which user chose
 
-
-    if len(user_request) == 1 and user_request[0] != '':
-        currency: str = user_request[0].upper()
-
-    elif len(user_request) == 2:
-        currency: str = user_request[0].upper()
+    if len(user_request) == 2:
         data: str = user_request[1].replace('.', '')
 
     api_url: str = f'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode={currency}&date={data}&json'
@@ -29,7 +30,10 @@ try:
     response: list = requests.get(api_url).json()
 
     if currency not in bank_currencies:
-        print(f'Invalid currency name: {currency}')
+        if currency == '':
+            print('Invalid currency name: None')
+        else:
+            print(f'Invalid currency name: {currency}')
 
     elif response == list or 'message' in response[0]:
         print(f'Invalid date {user_request[1]}')
